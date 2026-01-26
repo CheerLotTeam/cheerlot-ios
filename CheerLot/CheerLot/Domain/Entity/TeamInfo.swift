235b8665c829c1102ba4b7a975ccff26ba76f213
dlguszoo <lhjivy1024@gmail.com>
2026-01-26 16:51:55 +0900

    [#4] feat: TeamInfo Entity 구현

---
+++ CheerLot/CheerLot/Domain/Entity/TeamInfo.swift
@@ -0,0 +1,31 @@
+//
+//  TeamInfo.swift
+//  CheerLot
+//
+//  Created by 이현주 on 1/26/26.
+//
+
+import Foundation
+
+/// Team 정보
+struct TeamInfo: Identifiable, Hashable, Equatable {
+  let id: String
+  let shortName: String
+  let longName: String
+  let englishFullName: String
+  let slogan: String
+  
+  init(
+    id: String,
+    shortName: String,
+    longName: String,
+    englishFullName: String,
+    slogan: String
+  ) {
+    self.id = id
+    self.shortName = shortName
+    self.longName = longName
+    self.englishFullName = englishFullName
+    self.slogan = slogan
+  }
+}
