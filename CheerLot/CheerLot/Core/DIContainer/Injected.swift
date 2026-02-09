98fc3393d781d00fdbc20781ba211b632b318305
dlguszoo <lhjivy1024@gmail.com>
2026-02-10 04:30:17 +0900

    [#13] feat: Injected 프로퍼티 래퍼 구현

---
+++ CheerLot/CheerLot/Core/DIContainer/Injected.swift
@@ -0,0 +1,32 @@
+//
+//  Injected.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/9/26.
+//
+
+import Foundation
+
+/// DI Container에서 의존성을 주입하는 PropertyWrapper
+@propertyWrapper
+struct Injected<T> {
+    
+    private let type: T.Type
+    private let container: DIContainer
+    
+    var wrappedValue: T {
+        container.resolve(type)
+    }
+    
+    /// 기본 Container 사용
+    init(_ type: T.Type) {
+        self.type = type
+        self.container = DIContainer.shared
+    }
+    
+    /// 지정 Container 사용 (테스트용)
+    init(_ type: T.Type, container: DIContainer) {
+        self.type = type
+        self.container = container
+    }
+}
