26e64dd0abdcd8ebd761a64ec2539a2a937c70fd
dlguszoo <lhjivy1024@gmail.com>
2026-02-10 04:32:29 +0900

    [#13] feat: ViewModelFactory 구현

---
+++ CheerLot/CheerLot/Core/DIContainer/ViewModelFactory.swift
@@ -0,0 +1,22 @@
+//
+//  ViewModelFactory.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/9/26.
+//
+
+import Foundation
+
+final class ViewModelFactory {
+    
+    static let shared = ViewModelFactory()
+    
+    private init() {}
+    
+    // MARK: - Lineup
+//    func createLineupViewModel(team: TeamInfo) -> LineupViewModel {
+//        LineupViewModel()
+//    }
+    
+    // MARK: - Playback
+}
