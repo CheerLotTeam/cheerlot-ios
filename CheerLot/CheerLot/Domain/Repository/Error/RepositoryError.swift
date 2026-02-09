b2045ffa0d9eddbb8b1c090bf2a11f2e5f4eb6f5
dlguszoo <lhjivy1024@gmail.com>
2026-02-10 04:39:16 +0900

    [#13] feat: RepositoryError 구현

---
+++ CheerLot/CheerLot/Domain/Repository/Error/RepositoryError.swift
@@ -0,0 +1,31 @@
+//
+//  RepositoryError.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/10/26.
+//
+
+import Foundation
+
+enum RepositoryError: LocalizedError {
+    case fetchError
+    case notFound
+    case invalidData
+    case saveFailed
+    case dataAlreadyExist
+    
+    var errorDescription: String? {
+        switch self {
+        case.fetchError:
+            return "데이터를 불러오는 데 실패했습니다."
+        case .notFound:
+            return "데이터를 찾을 수 없습니다."
+        case .invalidData:
+            return "잘못된 데이터입니다."
+        case .saveFailed:
+            return "저장에 실패했습니다."
+        case .dataAlreadyExist:
+            return "이미 존재하는 데이터입니다."
+        }
+    }
+}
