c7a9c81e1c04a601c515f717daadb789165f9bf8
dlguszoo <lhjivy1024@gmail.com>
2026-02-09 15:42:12 +0900

    [#13] feat: MainRoute, ModalRoute 생성

---
+++ CheerLot/CheerLot/Core/Coordinator/MainRoute.swift
@@ -0,0 +1,21 @@
+//
+//  MainRoute.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/9/26.
+//
+
+import Foundation
+
+enum MainRoute: Hashable {
+    case settings
+    
+    // 지원
+    case serviceInfo
+    case makerInfo
+    
+    // 서비스 소개
+    case termsOfService
+    case privacyPolicy
+    case copyright
+}
---
+++ CheerLot/CheerLot/Core/Coordinator/ModalRoute.swift
@@ -0,0 +1,40 @@
+//
+//  ModalRoute.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/9/26.
+//
+
+import SwiftUI
+
+enum PresentationStyle {
+    case sheet
+    case fullScreen
+}
+
+enum ModalRoute: Identifiable {
+    // TODO: - 각 case 마다 넘겨주는 값(파라미터) 재설정
+    // Sheet 스타일
+    case cheerSongList
+    case lineupChange
+    case teamChange
+    case inquiry
+    case servicePage
+    
+    // FullScreen 스타일
+    case lineupPlayback
+    case basePlayback
+    
+    var id: String {
+      String(describing: self)
+    }
+    
+    var presentationStyle: PresentationStyle {
+        switch self {
+        case .cheerSongList, .lineupChange, .teamChange, .inquiry, .servicePage:
+            return .sheet
+        case .lineupPlayback, .basePlayback:
+            return .fullScreen
+        }
+    }
+}
