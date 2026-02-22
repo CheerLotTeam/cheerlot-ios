d209079b39089b4880487207afb1c7384307bdf3
dlguszoo <lhjivy1024@gmail.com>
2026-02-22 23:51:35 +0900

    [#14] feat: LineupChangeSheetView 생성

---
+++ CheerLot/CheerLot/Presentation/Lineup/View/LineupChangeSheetView.swift
@@ -0,0 +1,27 @@
+//
+//  LineupChangeSheetView.swift
+//  CheerLot
+//
+//  Created by 이현주 on 2/22/26.
+//
+
+import SwiftUI
+
+struct LineupChangeSheetView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .toolBar_editMode(
+            title: "선발 라인업") {
+                // TODO: - cancel 시,
+            } onCheck: {
+                // TODO: - check 시,
+            }
+
+    }
+}
+
+#Preview {
+    LineupChangeSheetView()
+}
