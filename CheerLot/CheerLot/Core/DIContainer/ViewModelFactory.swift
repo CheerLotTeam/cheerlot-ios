10fde12332a56361aacf4e4c02e43f7c27a99734
Seungjin Lee <sjin3687@gmail.com>
2026-02-22 18:47:30 +0900

    [#24] feat: playback, teamMembers ViewModel 생성

--- CheerLot/CheerLot/Core/DIContainer/ViewModelFactory.swift
+++ CheerLot/CheerLot/Core/DIContainer/ViewModelFactory.swift
@@ -8,15 +8,27 @@
 import Foundation
 
 final class ViewModelFactory {
-    
-    static let shared = ViewModelFactory()
-    
-    private init() {}
-    
-    // MARK: - Lineup
-//    func createLineupViewModel(team: TeamInfo) -> LineupViewModel {
-//        LineupViewModel()
-//    }
-    
-    // MARK: - Playback
+  
+  static let shared = ViewModelFactory()
+  
+  private init() {}
+  
+  // MARK: - Lineup
+  //    func createLineupViewModel(team: TeamInfo) -> LineupViewModel {
+  //        LineupViewModel()
+  //    }
+  
+  // MARK: - TeamMembers
+  func createTeamMembersViewModel(team: TeamInfo, audioPlayer: AudioPlaybackService) -> TeamMembersViewModel {
+    TeamMembersViewModel(audioPlayer: audioPlayer)
+  }
+  
+  // MARK: - Playback
+  func createPlaybackViewModel(
+    song: CheerSongInfo,
+    playerName: String,
+    audioPlayer: AudioPlaybackService
+  ) -> PlaybackViewModel {
+    PlaybackViewModel(song: song, playerName: playerName, audioPlayer: audioPlayer)
+  }
 }
