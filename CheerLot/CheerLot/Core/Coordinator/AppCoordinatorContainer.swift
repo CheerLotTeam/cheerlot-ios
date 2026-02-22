d740b68df8cd115b5b2a0d2df0eb282f4f400a41
Seungjin Lee <sjin3687@gmail.com>
2026-02-22 18:53:04 +0900

    [#24] feat: Audio Session 등록

--- CheerLot/CheerLot/Core/Coordinator/AppCoordinatorContainer.swift
+++ CheerLot/CheerLot/Core/Coordinator/AppCoordinatorContainer.swift
@@ -8,51 +8,52 @@
 import SwiftUI
 
 struct AppCoordinatorContainer: View {
-    
-    let team: TeamInfo
-    
-    @State private var coordinator = AppCoordinator()
-    
-    var body: some View {
-        NavigationStack(path: $coordinator.paths) {
-            MainTabView(team: team)
-                .navigationDestination(for: MainRoute.self) { route in
-                    coordinator.buildView(for: route)
-                }
+  
+  let team: TeamInfo
+  let audioPlayer: AudioPlaybackService
+  
+  @State private var coordinator = AppCoordinator()
+  
+  var body: some View {
+    NavigationStack(path: $coordinator.paths) {
+      MainTabView(team: team, audioPlayer: audioPlayer)
+        .navigationDestination(for: MainRoute.self) { route in
+          coordinator.buildView(for: route)
         }
-        .modal(item: $coordinator.modal) { route in
-            coordinator.buildModalView(for: route)
-        }
-        .environment(coordinator)
     }
+    .environment(coordinator)
+    .modal(item: $coordinator.modal) { route in
+      coordinator.buildModalView(for: route, audioPlayer: audioPlayer)
+    }
+  }
 }
 
 // MARK: - Modal Modifier Extension
 struct ModalModifier<Item: Identifiable, ModalContent: View>: ViewModifier {
-    
-    @Binding var item: Item?
-    let content: (Item) -> ModalContent
-    
-    func body(content mainContent: Content) -> some View {
-        if let item = item as? ModalRoute {
-            switch item.presentationStyle {
-            case .sheet:
-                return AnyView(
-                    mainContent
-                        .sheet(item: $item) { route in
-                            self.content(route)
-                        }
-                )
-            case .fullScreen:
-                return AnyView(
-                    mainContent
-                        .fullScreenCover(item: $item) { route in
-                            self.content(route)
-                        }
-                )
+  
+  @Binding var item: Item?
+  let content: (Item) -> ModalContent
+  
+  func body(content mainContent: Content) -> some View {
+    if let item = item as? ModalRoute {
+      switch item.presentationStyle {
+      case .sheet:
+        return AnyView(
+          mainContent
+            .sheet(item: $item) { route in
+              self.content(route)
             }
-        } else {
-            return AnyView(mainContent)
-        }
+        )
+      case .fullScreen:
+        return AnyView(
+          mainContent
+            .fullScreenCover(item: $item) { route in
+              self.content(route)
+            }
+        )
+      }
+    } else {
+      return AnyView(mainContent)
     }
+  }
 }
