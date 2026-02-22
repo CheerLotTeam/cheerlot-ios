9bd0e90d4c65b8457c6b6a8b4d050134b8546686
dlguszoo <lhjivy1024@gmail.com>
2026-02-22 13:29:41 +0900

    [#14] feat: lineupCellActions 모디파이어 구현

--- CheerLot/CheerLot/Core/Util/Extensions/View+.swift
+++ CheerLot/CheerLot/Core/Util/Extensions/View+.swift
@@ -140,4 +140,9 @@ extension View {
     ) -> some View {
         self.modifier(ModalModifier(item: item, content: content))
     }
+    
+    /// lineupView의 리스트 cellAction(swipe, context menu)을 바로 사용할 수 있는 확장메서드
+    func lineupCellActions() -> some View {
+        self.modifier(LineupCellActionsModifier())
+    }
 }
