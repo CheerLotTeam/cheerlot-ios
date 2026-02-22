acd7ea8e04e49024918384e013ba8e66f11e908b
Seungjin Lee <sjin3687@gmail.com>
2026-02-22 18:50:07 +0900

    [#24] feat: PlaybackView 구현

--- CheerLot/CheerLot/Presentation/Playback/View/PlaybackView.swift
+++ CheerLot/CheerLot/Presentation/Playback/View/PlaybackView.swift
@@ -9,47 +9,59 @@ import SwiftUI
 
 /// 전체 전수 명단에서 응원가를 재생하면 보여지는 뷰입니다.
 struct PlaybackView: View {
-
+  
   // MARK: - Properties
   let asset: TeamAssetVO
-
-  // TODO: - 지울 예정 (UI 목적)
-  @State private var isPlaying: Bool = false
-  @State private var currentTime: Double = 30
-  private let duration: Double = 90
-
+  
+  @State var viewModel: PlaybackViewModel
+  
+  init(asset: TeamAssetVO, viewModel: PlaybackViewModel) {
+    self.asset = asset
+    self.viewModel = viewModel
+  }
+  
   // MARK: - Body
   var body: some View {
+    VStack(spacing: 14) {
+      Capsule()
+        .fill(.gray200.opacity(0.6))
+        .frame(width: 60, height: 5)
+      mainView
+    }
+    .padding(.top, 60)
+    .background(asset.primary)
+    .onAppear { viewModel.onAppear() }
+    .onDisappear { viewModel.onDisappear() }
+  }
+}
+
+extension PlaybackView {
+  private var mainView: some View {
     VStack(spacing: 40) {
       header
       content
       footer
     }
-    .background(asset.primary)
-    .ignoresSafeArea()
   }
-}
-
-extension PlaybackView {
+  
   /// 선수 이름 + 응원가 종류
   private var header: some View {
     VStack(spacing: .zero) {
-      Text("김지찬")
+      Text(viewModel.playerName)
         .font(.B3)
         .foregroundStyle(.grayWhite)
-
-      Text("기본 응원가")
+      
+      Text(viewModel.title)
         .font(.SB8)
         .foregroundStyle(.gray200)
     }
-    .padding(.top, 90)
   }
-
+  
   /// 가사뷰
   private var content: some View {
     ScrollView(showsIndicators: true) {
       LazyVStack(alignment: .leading) {
-        Text("하이하이하이하이\n하이하이\n하이")
+        Text(viewModel.lyrics)
       }
       .multilineTextAlignment(.leading)
       .font(.B1_1)
@@ -57,7 +69,7 @@ extension PlaybackView {
     }
     .padding(.horizontal, 24)
   }
-
+  
   /// 재생바 + 컨트롤뷰
   private var footer: some View {
     VStack(spacing: 20) {
@@ -67,41 +79,41 @@ extension PlaybackView {
     .padding(.horizontal, 20)
     .padding(.bottom, 36)
   }
-
+  
   /// 재생바
   private var progressView: some View {
     VStack(spacing: 8) {
       PlaybackSeekBar(
-        value: $currentTime,
-        maxValue: duration,
-        onSeek: { _ in }
+        value: $viewModel.progress,
+        maxValue: viewModel.duration,
+        onSeek: { viewModel.seek(to: $0) }
       )
-
+      
       HStack {
-        Text("00:00")
+        Text(viewModel.progress.asTimeString)
         Spacer()
-        Text("01:30")
+        Text(viewModel.duration.asTimeString)
       }
       .font(.M5)
       .foregroundStyle(.gray300)
       .padding(.bottom, 4)
     }
   }
-
+  
   /// 컨트롤
   private var controlView: some View {
     HStack(spacing: 44) {
       playbackButton("backward.fill")
       playbackButton(
-        isPlaying ? "pause.fill" : "play.fill",
+        viewModel.isPlaying ? "pause.fill" : "play.fill",
         center: true
       ) {
-        isPlaying.toggle()
+        viewModel.togglePlayback()
       }
       playbackButton("forward.fill")
     }
   }
-
+  
   /// 같은 버튼 스타일을 씌우기 위한 함수
   private func playbackButton(
     _ systemName: String,
@@ -122,8 +134,4 @@ extension PlaybackView {
     }
     .buttonStyle(PlaybackButtonStyle(size: 56))
   }
-}
-
-#Preview {
-  PlaybackView(asset: TeamAssetVO(team: TeamDataSource.toEntity(.samsung)))
 }
