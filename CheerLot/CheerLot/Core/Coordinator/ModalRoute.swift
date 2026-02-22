ecee4591fca1c6fe40ddb30c723f2b5f2f74983d
Seungjin Lee <sjin3687@gmail.com>
2026-02-22 18:52:45 +0900

    [#24] feat: basePlayback 등록

--- CheerLot/CheerLot/Core/Coordinator/ModalRoute.swift
+++ CheerLot/CheerLot/Core/Coordinator/ModalRoute.swift
@@ -8,58 +8,59 @@
 import SwiftUI
 
 enum PresentationStyle {
-    case sheet
-    case fullScreen
+  case sheet
+  case fullScreen
 }
 
 enum ModalRoute: Identifiable {
-    // TODO: - 각 case 마다 넘겨주는 값(파라미터) 재설정
-    // Sheet 스타일
-    case cheerSongList
-    case lineupChange
-    case teamChange
-    case inquiry
-    case servicePage
-    
-    // FullScreen 스타일
-    case lineupPlayback
-    case basePlayback
-    
-    var id: String {
-      String(describing: self)
+  // TODO: - 각 case 마다 넘겨주는 값(파라미터) 재설정
+  // Sheet 스타일
+  case cheerSongList
+  case lineupChange
+  case teamChange
+  case inquiry
+  case servicePage
+  
+  // FullScreen 스타일
+  case lineupPlayback
+  case basePlayback(song: CheerSongInfo, playerName: String)
+  
+  var id: String {
+    String(describing: self)
+  }
+  
+  var presentationStyle: PresentationStyle {
+    switch self {
+    case .cheerSongList, .lineupChange, .teamChange, .inquiry, .servicePage:
+      return .sheet
+    case .lineupPlayback, .basePlayback:
+      return .fullScreen
     }
-    
-    var presentationStyle: PresentationStyle {
-        switch self {
-        case .cheerSongList, .lineupChange, .teamChange, .inquiry, .servicePage:
-            return .sheet
-        case .lineupPlayback, .basePlayback:
-            return .fullScreen
-        }
-    }
+  }
 }
 
 extension AppCoordinator {
-    @ViewBuilder
-    func buildModalView(for route: ModalRoute) -> some View {
-//        let factory = ViewModelFactory.shared
-        
-        // TODO: - View 넣기
-        switch route {
-        case let .cheerSongList:
-            Color.clear
-        case let .lineupChange:
-            Color.clear
-        case let .teamChange:
-            Color.clear
-        case let .inquiry:
-            Color.clear
-        case let .servicePage:
-            Color.clear
-        case let .lineupPlayback:
-            Color.clear
-        case let .basePlayback:
-            Color.clear
-        }
+  @ViewBuilder
+  func buildModalView(for route: ModalRoute, audioPlayer: AudioPlaybackService) -> some View {
+    let factory = ViewModelFactory.shared
+    
+    // TODO: - View 넣기
+    switch route {
+    case let .cheerSongList:
+      Color.clear
+    case let .lineupChange:
+      Color.clear
+    case let .teamChange:
+      Color.clear
+    case let .inquiry:
+      Color.clear
+    case let .servicePage:
+      Color.clear
+    case let .lineupPlayback:
+      Color.clear
+    case let .basePlayback(song, playerName):
+      let vm = factory.createPlaybackViewModel(song: song, playerName: playerName, audioPlayer: audioPlayer)
+      PlaybackView(asset: TeamAssetVO(team: TeamDataSource.toEntity(.kia)), viewModel: vm)
     }
+  }
 }
