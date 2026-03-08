e1ef40f04b4c591543c848fab60eb7cc33b7cb0e
dlguszoo <lhjivy1024@gmail.com>
2026-03-09 01:48:16 +0900

    [#18] chore: playCard 에셋추가 및 Image 폴더링 정리

---
+++ CheerLot/CheerLot/Presentation/Lineup/View/LineupPlaybackView.swift
@@ -0,0 +1,18 @@
+//
+//  LineupPlaybackView.swift
+//  CheerLot
+//
+//  Created by 이현주 on 3/9/26.
+//
+
+import SwiftUI
+
+struct LineupPlaybackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineupPlaybackView()
+}
