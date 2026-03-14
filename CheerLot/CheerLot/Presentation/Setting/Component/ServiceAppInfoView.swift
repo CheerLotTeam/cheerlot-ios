ec0bbef2e37dcc283e5812f4c31998b4de37fd09
Seungjin Lee <sjin3687@gmail.com>
2026-03-14 23:49:36 +0900

    [#37] feat: ServiceAppInfoView 기능 구현

--- CheerLot/CheerLot/Presentation/Setting/Component/ServiceAppInfoView.swift
+++ CheerLot/CheerLot/Presentation/Setting/Component/ServiceAppInfoView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 /// 서비스 약관 (이용약관, 개인정보처리방침, 저작권 법적고지)에서 쓰이는 공통 화면입니다.
 struct ServiceAppInfoView: View {
-
+  @Environment(AppCoordinator.self) private var coordinator
+  
   let title: String
   let text: String
 
@@ -20,10 +21,11 @@ struct ServiceAppInfoView: View {
           .font(.R2)
           .foregroundStyle(.gray500)
       }
+      .padding(.horizontal, 20)
     }
-    .navigationBarBackButtonHidden(true)
+    .toolbar(.hidden, for: .tabBar)
     .navigationBar_backWithTitle(title: title) {
-
+      coordinator.pop()
     }
   }
 }
