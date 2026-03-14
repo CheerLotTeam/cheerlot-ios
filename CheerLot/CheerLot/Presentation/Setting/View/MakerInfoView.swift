d759ba01a9d38ec43a7ccdb549e18622e7a248e2
Seungjin Lee <sjin3687@gmail.com>
2026-03-14 23:48:56 +0900

    [#37] feat: MakerInfoView 기능 구현

--- CheerLot/CheerLot/Presentation/Setting/View/MakerInfoView.swift
+++ CheerLot/CheerLot/Presentation/Setting/View/MakerInfoView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 /// 쳐랏 팀 화면입니다.
 struct MakerInfoView: View {
-
+  @Environment(AppCoordinator.self) private var coordinator
+  @Environment(\.openURL) private var openURL
+  
   // MARK: - Body
   var body: some View {
     VStack {
@@ -18,31 +20,31 @@ struct MakerInfoView: View {
         onTap: { index in
           let menus = MakerInfoMenu.allCases
           guard menus.indices.contains(index) else { return }
-          serviceInfoTap(menus[index])
+          makerInfoTap(menus[index])
         }
       )
 
       Spacer()
     }
     .padding(.horizontal, 20)
+    .toolbar(.hidden, for: .tabBar)
     .navigationBar_backWithTitle(title: "쳐랏 팀") {
-      // 네비게이션 연결
+      coordinator.pop()
     }
   }
 }
 
-// MARK: - 이후 옮길 예정
+// MARK: - Actions
 extension MakerInfoView {
-  /// 메뉴 탭 처리
-  private func serviceInfoTap(_ menu: MakerInfoMenu) {
+  private func makerInfoTap(_ menu: MakerInfoMenu) {
     switch menu {
     case .instagram:
-      // coordinator.modal = .servicePage
-      print("인스타그램 이동")
+      guard let url = URL(string: Constants.instagramURL) else { return }
+      openURL(url)
 
     case .goToReview:
-      // coordinator.paths.append(.termsOfService)
-      print("앱스토어로 이동")
+      guard let url = URL(string: Constants.appStoreURL) else { return }
+      openURL(url)
     }
   }
 }
