e44c61ecf26436df30f10b0e08d686e703e3378a
Hyeon-Ju <lhjivy1024@gmail.com>
2026-03-15 15:03:59 +0900

    [#39] fix: SplashView Lottie로 교체

--- CheerLot/CheerLot/Presentation/AppFlow/View/SplashView.swift
+++ CheerLot/CheerLot/Presentation/AppFlow/View/SplashView.swift
@@ -5,55 +5,51 @@
 //  Created by 이승진 on 7/10/25.
 //
 
-import AVKit
 import SwiftUI
+import Lottie
 
 /// 앱 실행시 보여주는 스플래시 화면
 struct SplashView: View {
   // TODO: - 분리 예정
   @EnvironmentObject private var remoteConfigChecker: RemoteConfigChecker
+  let onAnimationComplete: () -> Void
 
-  @State private var player = AVPlayer(
-    url: Bundle.main.url(forResource: "splash", withExtension: "mp4")!
-  )
-
-  private enum Constants {
-    static let splashDuration: UInt64 = 1_250_000_000  // 1.25초
-  }
-
-  var body: some View {
-    Group {
-      VideoPlayer(player: player)
-        .disabled(true)
-        .overlay(Color.clear)
-        .ignoresSafeArea()
-        .task {
-          player.play()
+    var body: some View {
+        LottieView {
+            try await DotLottieFile.named("cheerlot_splash")
         }
+        .playing()
+        .animationDidFinish { finished in
+            if finished {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    onAnimationComplete()
+                }
+            }
+        }
+        .ignoresSafeArea()
         // foreground로 복귀할 때마다 checkVersion 함수를 실행
         .onReceive(
-          NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)
+            NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)
         ) { _ in
-          Task {
-            await remoteConfigChecker.fetchRemoteConfig()
-          }
+            Task {
+                await remoteConfigChecker.fetchRemoteConfig()
+            }
+        }
+        .alert("최신 업데이트 안내", isPresented: $remoteConfigChecker.shouldForceUpdate) {
+            Button("확인") {
+                openAppStore()
+            }
+        } message: {
+            Text("안정적인 서비스 사용을 위해\n최신 버전으로 업데이트해 주세요")
+        }
+        .alert("서비스 점검 안내", isPresented: $remoteConfigChecker.isServerChecking) {
+            Button("확인") {
+                exitApp()
+            }
+        } message: {
+            Text(remoteConfigChecker.serverCheckingMessage)
         }
     }
-    .alert("최신 업데이트 안내", isPresented: $remoteConfigChecker.shouldForceUpdate) {
-      Button("확인") {
-        openAppStore()
-      }
-    } message: {
-      Text("안정적인 서비스 사용을 위해\n최신 버전으로 업데이트해 주세요")
-    }
-    .alert("서비스 점검 안내", isPresented: $remoteConfigChecker.isServerChecking) {
-      Button("확인") {
-        exitApp()
-      }
-    } message: {
-      Text(remoteConfigChecker.serverCheckingMessage)
-    }
-  }
 
   private func openAppStore() {
     if let url = URL(string: "itms-apps://itunes.apple.com/app/id6748527115") {
