f9b3e97f2fd4a0ac3ac34a631de348af03127b0f
Hyeon-Ju <lhjivy1024@gmail.com>
2026-03-18 02:02:01 +0900

    [#39] fix: UIScreen에서 @MainActor struct 밖으로 빼기

--- CheerLot/CheerLot/Core/Util/Extensions/UIScreen+.swift
+++ CheerLot/CheerLot/Core/Util/Extensions/UIScreen+.swift
@@ -7,16 +7,16 @@
 
 import UIKit
 
+@MainActor
 extension UIScreen {
-    @MainActor
-    static var current: CGRect {
-        let activeScene = UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .first { $0.activationState == .foregroundActive }
-        
-        let keyWindow = activeScene?.windows.first(where: \.isKeyWindow)
-        return keyWindow?.screen.bounds ?? UIScreen.main.bounds
-    }
+  static var current: CGRect {
+    let activeScene = UIApplication.shared.connectedScenes
+      .compactMap { $0 as? UIWindowScene }
+      .first { $0.activationState == .foregroundActive }
+
+    let keyWindow = activeScene?.windows.first(where: \.isKeyWindow)
+    return keyWindow?.screen.bounds ?? UIScreen.main.bounds
+  }
 
   /// 화면 너비
   static var width: CGFloat {
