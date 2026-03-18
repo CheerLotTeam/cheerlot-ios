3cf5b662700a16a4b28137a76cdcdeafd9367abf
Hyeon-Ju <lhjivy1024@gmail.com>
2026-03-18 18:40:41 +0900

    [#43] chore: Presentation 폴더링 재구성

--- CheerLot/WatchCheerLot Watch App/Presentation/Onboarding/WatchOnboardingView.swift
+++ CheerLot/WatchCheerLot Watch App/Presentation/Onboarding/WatchOnboardingView.swift
@@ -9,7 +9,23 @@ import SwiftUI
 
 struct WatchOnboardingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      ZStack {
+        Rectangle()
+          .fill(.ultraThinMaterial)
+          .ignoresSafeArea()
+
+        VStack(spacing: 2) {
+          Text("직관 집중 ON")
+                .font(.SB7)
+          Text("방해금지 모드를 켜주세요")
+                .font(.M6)
+            
+            Button("Tap Me") { // The string is the button's text
+                            // Code to execute when the button is tapped
+                            print("Button was tapped!")
+                        }
+        }
+      }
     }
 }
 
