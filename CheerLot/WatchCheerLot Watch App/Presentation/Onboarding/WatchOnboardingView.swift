725324e6389a0e0b700d4d7b52975c06936bc4c1
Hyeon-Ju <lhjivy1024@gmail.com>
2026-03-18 15:19:13 +0900

    [#43] chore: watchOS 프로젝트 파일 정리 (legacy, asset, font)

---
+++ CheerLot/WatchCheerLot Watch App/Presentation/Onboarding/WatchOnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  WatchOnboardingView.swift
+//  WatchCheerLot Watch App
+//
+//  Created by 이현주 on 3/18/26.
+//
+
+import SwiftUI
+
+struct WatchOnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WatchOnboardingView()
+}
