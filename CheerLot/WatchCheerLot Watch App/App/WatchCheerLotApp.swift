0753745ee041c8d7b9ae42b3fcfd05b72549b4b1
dlguszoo <lhjivy1024@gmail.com>
2026-03-19 20:44:42 +0900

    [#43] feat: App 파일 init 및 viewModel 선언 추가

--- CheerLot/WatchCheerLot Watch App/App/WatchCheerLotApp.swift
+++ CheerLot/WatchCheerLot Watch App/App/WatchCheerLotApp.swift
@@ -9,10 +9,18 @@ import SwiftUI
 
 @main
 struct WatchCheerLot_Watch_AppApp: App {
+    
+  @State private var lineupViewModel: LineupViewModel
+    
+  init() {
+    DIContainer.shared.assemble()
+    DIContainer.shared.resolve(WatchConnectivityRepository.self).activate()
+    _lineupViewModel = State(initialValue: ViewModelFactory.shared.createLineupViewModel())
+  }
 
   var body: some Scene {
     WindowGroup {
-        LineupView()
+        LineupView(viewModel: lineupViewModel)
     }
   }
 }
