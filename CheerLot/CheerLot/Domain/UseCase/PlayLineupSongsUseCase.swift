6f4a1d6708af5b32e22dc103fab7d5976a577bfd
Seungjin Lee <sjin3687@gmail.com>
2026-03-19 18:41:28 +0900

    [#44] feat: PlayLineupSongsUseCase 구현

---
+++ CheerLot/CheerLot/Domain/UseCase/PlayLineupSongsUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  PlayLineupSongsUseCase.swift
+//  CheerLot
+//
+//  Created by 이승진 on 3/18/26.
+//
+
+import Foundation
+
+protocol PlayLineupSongsUseCase {
+  var isPlaying: Bool { get }
+  var currentTime: Double { get }
+  var duration: Double { get }
+  var currentIndex: Int { get }
+
+  func playQueue(
+    _ songs: [CheerSongInfo],
+    playerNames: [String],
+    startAt index: Int
+  )
+
+  func play(at index: Int)
+  func playNext()
+  func playPrevious()
+  func pause()
+  func resume()
+  func toggle()
+  func stop()
+  func seek(_ seconds: Double)
+
+  func observeTime(
+    every interval: Double,
+    queue: DispatchQueue?,
+    _ handler: @escaping (Double) -> Void
+  ) -> Any
+
+  func removeObserver(_ token: Any)
+}
