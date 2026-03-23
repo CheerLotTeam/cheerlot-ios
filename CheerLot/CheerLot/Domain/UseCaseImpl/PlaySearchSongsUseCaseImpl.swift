70e53da70e1345c5f509096cd544e2106357064c
Seungjin Lee <sjin3687@gmail.com>
2026-03-23 23:15:37 +0900

    [#50] fix: 인덱스 계산 수정

--- CheerLot/CheerLot/Domain/UseCaseImpl/PlaySearchSongsUseCaseImpl.swift
+++ CheerLot/CheerLot/Domain/UseCaseImpl/PlaySearchSongsUseCaseImpl.swift
@@ -19,7 +19,7 @@ final class PlaySearchSongsUseCaseImpl: PlaySearchSongsUseCase {
     allResults: [SearchResultVO],
     coverImageName: String?
   ) {
-    let samePlayerRows = allResults.filter { $0.playerId == selectedResult.playerId }
+    let samePlayerRows = allResults.filter { $0.playerId == selectedResult.playerId && $0.song != nil }
     let songs = samePlayerRows.compactMap(\.song)
 
     guard !songs.isEmpty else { return }
