af2f798f37a849e2153846e65d1df00ff6da75cc
Seungjin Lee <sjin3687@gmail.com>
2026-04-01 19:09:33 +0900

    [#67] add: Widget Color & Image 추가

---
+++ CheerLot/WidgetCheerLot/Core/DesignSystem/Color+.swift
@@ -0,0 +1,8 @@
+//
+//  Color+.swift
+//  CheerLot
+//
+//  Created by 이승진 on 4/1/26.
+//
+
+import Foundation
---
+++ CheerLot/WidgetCheerLot/Presentation/Common/ValueObject/WidgetTeamAssetVO.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetTeamAssetVO.swift
+//  CheerLot
+//
+//  Created by 이승진 on 4/1/26.
+//
+
+import Foundation
