9f8045b02f1330b35445a112d9bcd58599dfca5e
dlguszoo <lhjivy1024@gmail.com>
2026-04-05 19:17:36 +0900

    [#66] feat: TeamGameScheduleInfo Entity 구현

---
+++ CheerLot/CheerLot/Domain/Entity/Team/TeamGameScheduleInfo.swift
@@ -0,0 +1,24 @@
+//
+//  TeamGameScheduleInfo.swift
+//  CheerLot
+//
+//  Created by 이현주 on 4/5/26.
+//
+
+import Foundation
+
+struct TeamGameScheduleInfo {
+    let id: TeamID
+    var recentGames: [GameScheduleInfo]
+}
+
+struct GameScheduleInfo {
+    let date: String
+    let hasGame: Bool
+    var opponentTeamId: TeamID?
+    var isHome: Bool?
+    var starterPitcherName: String?
+    var opponentStarterPitcherName: String?
+}
+
+
