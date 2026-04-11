19b257f099c126cbff190ceca0940ca8a77cf727
Seungjin Lee <sjin3687@gmail.com>
2026-04-12 02:22:17 +0900

    [#71] feat: songTitle 추가

--- CheerLot/CheerLot/Data/Storage/Local/UserDefaultsKey.swift
+++ CheerLot/CheerLot/Data/Storage/Local/UserDefaultsKey.swift
@@ -15,6 +15,7 @@ enum UserDefaultsKey {
 
   enum Widget {
     static let playerName = "widget.playerName"
+    static let songTitle = "widget.songTitle"
     static let totalSongCount = "widget.totalSongCount"
     static let gameSchedule = "widget.gameSchedule"
   }
