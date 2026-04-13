e630728f9db5301c3d378d20c9f351c22089f17c
dlguszoo <lhjivy1024@gmail.com>
2026-04-13 14:10:21 +0900

    [#77] feat: showLineup시, 최근 라인업 데이터를 보여주기 위한 LineupData Entity에 recentGameInfo 필드 추가

--- CheerLot/CheerLot/Domain/Entity/Team/LineupData.swift
+++ CheerLot/CheerLot/Domain/Entity/Team/LineupData.swift
@@ -11,4 +11,5 @@ struct LineupData {
   let gameInfo: TeamGameInfo
   let lineupPlayers: [PlayerInfo]
   let opponentTeamId: TeamID?
+  let recentGameInfo: TeamGameInfo? // lineupUpdatedToday=false일 때, teamData.gameInfo (최근 완료된 경기 정보). showLineup=true 상태에서 사용.
 }
